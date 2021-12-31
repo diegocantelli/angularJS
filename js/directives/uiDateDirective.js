5714e4e455422f146b37a6f50b50510f1c447c27
Diego Cantelli <diegocantelli19@gmail.com>
2021-12-31 17:41:37 -0300

    finalizando a implementação da máscara

--- js/directives/uiDateDirective.js
+++ js/directives/uiDateDirective.js
@@ -4,8 +4,23 @@ angular.module("listaTelefonica").directive("uiDate", function(){
         //link -> executada depois do template ter sido carregado. Podemos utilizar para interagir com o DOM e eventos
         //ctrl -> é o controle de ngMolde, através dele poderemos acessar a api de ngModel
         link: function(scope, element, attrs, ctrl){
+
+            var _formatDate = function(date){
+                date = date.replace(/[^0-9]+/g,"");
+
+                if(date.length > 2){
+                    date = date.substring(0,2) + "/" + date.substring(2);
+                }
+
+                if(date.length > 5){
+                    date = date.substring(0,5) + "/" + date.substring(5,9);
+                }
+
+                return date;
+            }
             element.bind("keyup", function(){
-                console.log(ctrl.$viewValue);
+                ctrl.$setViewValue(_formatDate(ctrl.$viewValue));
+                ctrl.$render();
             });
         }
     };
