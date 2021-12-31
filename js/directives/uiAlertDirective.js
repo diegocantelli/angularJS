2a4dc90efca587979d12eaf0d3634c636cbfe258
Diego Cantelli <diegocantelli19@gmail.com>
2021-12-31 13:08:45 -0300

    Identificando o scopo atual entre aplicação e diretivas

--- js/directives/uiAlertDirective.js
+++ js/directives/uiAlertDirective.js
@@ -1,6 +1,7 @@
 // Pode-se usar diretivas para reutilizar componentes visuais
 angular.module("listaTelefonica").directive("uiAlert", function(){
     return {
-        templateUrl: "../../views/alert.html"
+        templateUrl: "../../views/alert.html",
+        restrict: "AE" //o elemento poderá ser invocado via <div ui-alert></div> ou <ui-alert></ui-alert>
     };
 });
