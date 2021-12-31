cbdf9017792214776e6894f97f0f03331ca004a3
Diego Cantelli <diegocantelli19@gmail.com>
2021-12-31 13:02:46 -0300

    extraindo a view inline da diretiva para uma view em um arquivo separado

--- js/directives/uiAlertDirective.js
+++ js/directives/uiAlertDirective.js
@@ -1,6 +1,6 @@
 // Pode-se usar diretivas para reutilizar componentes visuais
 angular.module("listaTelefonica").directive("uiAlert", function(){
     return {
-        template: "<div>UiAlert</div>"
+        templateUrl: "../../views/alert.html"
     };
 });
