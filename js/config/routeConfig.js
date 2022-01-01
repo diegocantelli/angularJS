827b8ad0ed815bdb7a1ab2483d9852f946d97e25
Diego Cantelli <diegocantelli19@gmail.com>
2022-01-01 13:43:14 -0300

    Criando a navegação através da tela de contatos para a tela de novo contato

--- js/config/routeConfig.js
+++ js/config/routeConfig.js
@@ -1,5 +1,14 @@
 angular.module("listaTelefonica").config(function($routeProvider){
+    // O nome da rota é case sensitive
    $routeProvider.when("/contatos", {
-       templateUrl: "../../views/contatos.html"
+       templateUrl: "../../views/contatos.html",
+       //dessa forma não é mais necessário linkar a controller na view, pode ser feito direto na config da rota
+       controller: "listaTelefonicaCtrl"
    });
+
+   $routeProvider.when("/novocontato", {
+    templateUrl: "../../views/novoContato.html",
+    //dessa forma não é mais necessário linkar a controller na view, pode ser feito direto na config da rota
+    controller: "listaTelefonicaCtrl"
+});
 });
